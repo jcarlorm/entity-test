bad385e59190a7c1ee382d95a02f432bd1ae7359
Jose Carlo <jcarlorm.dev@gmail.com>
2019-02-20 00:13:14 -0600

    pruebas e2e

---
+++ E2E.uitest/UITestLogin.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
+
+namespace E2E.uitest
+{
+    /// <summary>
+    /// Descripción resumida de CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class UITestLogin
+    {
+        public UITestLogin()
+        {
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+
+
+            // Para generar código para esta prueba, seleccione "Generar código para prueba automatizada de IU" en el menú contextual y seleccione uno de los elementos de menú.
+            this.UIMap.TestLogin();
+            this.UIMap.Acepto();
+
+            //WinEdit  uITxtNombreEdit = this.UIMap.UILoginWindow.UITxtNombreWindow.UITxtNombreEdit;
+            //WinEdit uITxtPasswordEdit = this.UIMap.UILoginWindow.UITxtPasswordWindow.UITxtPasswordEdit;
+            //WinButton uILoginButton = this.UIMap.UILoginWindow.UILoginWindow1.UILoginButton;
+            //WinButton uIAceptarButton = this.UIMap.UIAceptarWindow.UIAceptarButton;
+
+            //uITxtNombreEdit.Text = "Prueba";
+            //uITxtPasswordEdit.SetFocus();
+            //uITxtPasswordEdit.Text = "clave";
+            //uILoginButton.SetFocus();
+
+            //// Escribir '{Space}' en botón 'Login'
+            //Keyboard.SendKeys(uILoginButton, "{space}", ModifierKeys.None);
+            //// Clic 'Aceptar' botón
+            //if (uIAceptarButton.TryFind())
+            //{
+            //    Assert.IsTrue(true);
+            //}
+            //Assert.IsTrue(false);
+
+
+            //Mouse.Click(uIAceptarButton, new Point(28, 15));
+
+        }
+
+        #region Atributos de prueba adicionales
+
+        // Puede usar los siguientes atributos adicionales conforme escribe las pruebas:
+
+        ////Use TestInitialize para ejecutar el código antes de ejecutar cada prueba 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // Para generar código para esta prueba, seleccione "Generar código para prueba automatizada de IU" en el menú contextual y seleccione uno de los elementos de menú.
+        //}
+
+        ////Use TestCleanup para ejecutar el código después de ejecutar cada prueba
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // Para generar código para esta prueba, seleccione "Generar código para prueba automatizada de IU" en el menú contextual y seleccione uno de los elementos de menú.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Obtiene o establece el contexto de las pruebas que proporciona
+        ///información y funcionalidad para la serie de pruebas actual.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+
+
+  
+}
---
+++ UI.proyecto/frmLogin.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace UI.proyecto
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(33, 23);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(201, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(33, 60);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(201, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(33, 95);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 228);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
